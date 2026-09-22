4387c72e5cf2a5be8f049dbd95bca33d48a76d0d
Marc Pabalan <marclaurencepabs@gmail.com>
2026-09-22 15:27:18 +0800

    Implemented RBAC foundation and protected routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/citizen/dashboard', function () {
+    return 'Citizen Dashboard';
+})->middleware(['auth', 'role:citizen']);
+
+Route::get('/personnel/dashboard', function () {
+    return 'Personnel Dashboard';
+})->middleware(['auth', 'role:personnel']);
